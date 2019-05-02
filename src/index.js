bdfecee6d76b85c84e1e0a3abb09dcc2f8fb6d74
github username <github email address>
2019-05-02 08:09:01 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-})
+
+const getDog = function(){
+   return fetch('http://localhost:3000/dogs')
+        .then(resp=>resp.json())
+}
+
+//get one Dog
+const registeredDog = function(dog){
+     const dogTable = document.querySelector('#table-body')
+     const dogTableEl = document.createElement('tr')
+     dogTableEl.innerHTML = `
+
+        <td> ${dog.name}</td> 
+        <td> ${dog.breed}</td> 
+        <td> ${dog.sex}</td> <td>
+        <button id="edit">Edit</button></td>
+     `
+     dogTable.append(dogTableEl)
+}
+
+//get multiple Dogs
+
+const registeredDogs = function(dogs){
+    dogs.forEach(registeredDog)
+}
+
+getDog()
+    .then(registeredDogs)
+
+
